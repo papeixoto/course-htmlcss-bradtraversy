7036c9d0af9c67be87af4033579851b2d898dcff
Paulo <pazevedopeixoto@xpand-it.com>
2020-10-17 17:28:41 +0100

    progress so far

---
+++ edgeledger_website/js/main.js
@@ -0,0 +1,39 @@
+var map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: {
+      lat: 40.9798,
+      lng: -8.5128
+    },
+    zoom: 16
+  });
+
+  const marker = new google.maps.Marker({
+    position: {
+      lat: 40.9798,
+      lng: -8.5128
+    },
+    map: map
+  });
+}
+
+// Sticky menu background
+window.addEventListener('scroll', function () {
+  if (window.scrollY > 150) {
+    document.querySelector('#navbar').style.opacity = 0.9;
+  } else {
+    document.querySelector('#navbar').style.opacity = 1;
+  }
+})
+
+$('#navbar a, .btn').on('click', function (event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+    const hash = this.hash;
+    $('html, body').animate({
+        scrollTop: $(hash).offset().top - 65
+      },
+      800);
+  }
+});
